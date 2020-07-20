5e6bd93aa169e4269abc957d5bf4f0bc9fe164aa
Michał Budziak <michal.mariusz.b@gmail.com>
2020-07-20 21:59:54 +0200

    WIP

--- scripts/compile_armv6.sh
+++ scripts/compile_armv6.sh
@@ -4,7 +4,11 @@ IFS=$'\n\t'
 
 # https://github.com/mdirkse/rust_armv6
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORKDIR="/work"
+IMAGE_NAME="raspberrypi-build"
+
+docker build -t ${IMAGE_NAME} -f "$DIR/Dockerfile" .
 docker run --rm -t \
        -u "$(id -u):$(id -g)" \
        -e "HOME=${WORKDIR}" \
@@ -12,5 +16,4 @@ docker run --rm -t \
        -w "${WORKDIR}" \
        -v "$(pwd):${WORKDIR}" \
        -v "${HOME}/.cargo/registry:/usr/local/cargo/registry" \
-       mdirkse/rust_armv6:latest
-
+       ${IMAGE_NAME}
