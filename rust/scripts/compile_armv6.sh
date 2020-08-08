d487c8299b66e519e888ef3424c3ec65027ab8f6
Michał Budziak <michal.mariusz.b@gmail.com>
2020-08-08 23:58:29 +0200

    Move directories

--- rust/scripts/compile_armv6.sh
+++ rust/scripts/compile_armv6.sh
@@ -4,15 +4,15 @@ IFS=$'\n\t'
 
 # https://github.com/mdirkse/rust_armv6
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIRECTORY=$(cd `dirname $0` && pwd)
 WORKDIR="/work"
 IMAGE_NAME="raspberrypi-build"
 
-docker build -t ${IMAGE_NAME} -f "$DIR/Dockerfile" .
+docker build -t ${IMAGE_NAME} -f "$DIRECTORY/Dockerfile" .
 docker run --rm -t \
        -e "HOME=${WORKDIR}" \
        -e "PKG_CONFIG_ALLOW_CROSS=1" \
        -w "${WORKDIR}" \
-       -v "$(pwd):${WORKDIR}" \
+       -v "${DIRECTORY}/..:${WORKDIR}" \
        -v "${HOME}/.cargo/registry:/usr/local/cargo/registry" \
        ${IMAGE_NAME}
