a9bf2493ad4bee972057d00006110a1d5201e4cc
Michał Budziak <michal.mariusz.b@gmail.com>
2020-11-08 16:30:15 +0100

    Split code

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 use dbus::blocking::Connection;
-use dbus_crossroads::Crossroads;
+use dbus_crossroads::{Crossroads, Context, MethodErr, IfaceToken};
 use serde::{Deserialize, Serialize};
 use serde_json::{to_string as json_encode};
 
@@ -16,43 +16,50 @@ struct WifiOnResponse  {
     code: String,
 }
 
-fn main() {
-    let result = server();
-
-    match result {
-        Ok(_) => println!("Server done"),
-        Err(e) => panic!("Problem with running a client: {:?}", e),
-    };
+fn main() -> () {
+    match server() {
+        Ok(_) => unreachable!(),
+        Err(e) => panic!("{:?}", e),
+    }
 }
 
 fn server() -> Result<(), Box<dyn Error>> {
     let c = Connection::new_session()?;
     c.request_name("pl.digitalradio", false, true, false)?;
 
-    let mut cr = Crossroads::new();
-
-    let iface_token = cr.register("pl.digitalradio", |b| {
-        b.method("wifi_on", ("data",), ("response",), |_, _, (data,): (String,)| {
-            let parsed_data: WifiOnData = match serde_json::from_str(data.as_str()) {
-                    Ok(d) => d,
-                    Err(_) => return Err(("pl.digitalradio.JsonDecodeError", "Could not decode args").into()),
-            };
-
-            println!("Connect to Wi-Fi [{}]", parsed_data.ssid);
-
-            let response = WifiOnResponse {
-                code: "ok".to_owned(),
-            };
-            let response_data = match json_encode(&response) {
-                Ok(d) => d,
-                Err(_) => return Err(("pl.digitalradio.JsonEncodeError", "Could not encode response").into()),
-            };
-            Ok((response_data,))
-        });
-    });
-
-    cr.insert("/malina", &[iface_token], ());
+    let cr = create_crossroad();
     cr.serve(&c)?;
 
     unreachable!()
+}
+
+fn create_crossroad() -> Crossroads {
+    let mut cr = Crossroads::new();
+    let iface_token = create_interface(&mut cr);
+    cr.insert("/malina", &[iface_token], ());
+    cr
+}
+
+fn create_interface(cr: &mut Crossroads) -> IfaceToken<()> {
+    cr.register("pl.digitalradio", |b| {
+        b.method("wifi_on", ("data", ), ("response", ), handle_wifi_on);
+    })
+}
+
+fn handle_wifi_on(_ctx: &mut Context, _data: &mut (), (body,): (String,)) -> Result<(String,), MethodErr> {
+    let parsed_data: WifiOnData = match serde_json::from_str(body.as_str()) {
+        Ok(d) => d,
+        Err(_) => return Err(("pl.digitalradio.JsonDecodeError", "Could not decode args").into()),
+    };
+
+    println!("Connect to Wi-Fi [{}]", parsed_data.ssid);
+
+    let response = WifiOnResponse {
+        code: "ok".to_owned(),
+    };
+    let response_data = match json_encode(&response) {
+        Ok(d) => d,
+        Err(_) => return Err(("pl.digitalradio.JsonEncodeError", "Could not encode response").into()),
+    };
+    Ok((response_data,))
 }
