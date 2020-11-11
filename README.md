a506d4dfa54ea98c0b3b99024a4fb978df9f9071
Michał Budziak <michal.mariusz.b@gmail.com>
2020-11-11 23:49:48 +0100

    Shell available from different threads

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Malina
 ===
+
+
+### Useful links
+* https://users.rust-lang.org/t/borrow-in-static-closure/35493
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,15 @@
+use std::env;
 use std::error::Error;
+use std::sync::{Arc, Mutex};
 
 use dbus::blocking::Connection;
-use dbus_crossroads::{Crossroads, Context, MethodErr, IfaceToken};
+use dbus_crossroads::{Context, Crossroads, MethodErr};
 use serde::{Deserialize, Serialize};
-use serde_json::{to_string as json_encode};
+use serde_json::to_string as json_encode;
+
+use shell::Shell;
+
+mod shell;
 
 #[derive(Serialize, Deserialize)]
 struct WifiOnData {
@@ -16,50 +22,55 @@ struct WifiOnResponse  {
     code: String,
 }
 
-fn main() -> () {
-    match server() {
-        Ok(_) => unreachable!(),
-        Err(e) => panic!("{:?}", e),
+pub struct WifiOnHandler {
+    shell: Arc<Mutex<Shell>>,
+}
+
+impl WifiOnHandler {
+    pub fn new(shell: Arc<Mutex<Shell>>) -> WifiOnHandler {
+        WifiOnHandler { shell }
+    }
+
+    pub fn handle(&self, _ctx: &mut Context, body: String) -> Result<(String,), MethodErr> {
+        let parsed_data: WifiOnData = match serde_json::from_str(body.as_str()) {
+            Ok(d) => d,
+            Err(_) => return Err(("pl.digitalradio.JsonDecodeError", "Could not decode args").into()),
+        };
+
+        println!("Connect to Wi-Fi [{}]", parsed_data.ssid);
+        let shell = self.shell.lock().unwrap();
+        shell.connect_wifi(&parsed_data.ssid, &parsed_data.password);
+
+        let response = WifiOnResponse {
+            code: "ok".to_owned(),
+        };
+        let response_data = match json_encode(&response) {
+            Ok(d) => d,
+            Err(_) => return Err(("pl.digitalradio.JsonEncodeError", "Could not encode response").into()),
+        };
+        Ok((response_data,))
     }
 }
 
-fn server() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let shell = Arc::new(Mutex::new(Shell::new(args[1].to_owned())));
+
     let c = Connection::new_session()?;
     c.request_name("pl.digitalradio", false, true, false)?;
 
-    let cr = create_crossroad();
+    let mut cr = Crossroads::new();
+
+    let iface_token = cr.register("pl.digitalradio", |b| {
+        b.method("wifi_on", ("data", ), ("response", ), |_ctx: &mut Context, _data: &mut WifiOnHandler, (body, ): (String, )| {
+            _data.handle(_ctx, body)
+        });
+    });
+
+
+    let wifi_on_handler = WifiOnHandler::new(Arc::clone(&shell));
+    cr.insert("/malina", &[iface_token], wifi_on_handler);
     cr.serve(&c)?;
 
     unreachable!()
-}
-
-fn create_crossroad() -> Crossroads {
-    let mut cr = Crossroads::new();
-    let iface_token = create_interface(&mut cr);
-    cr.insert("/malina", &[iface_token], ());
-    cr
-}
-
-fn create_interface(cr: &mut Crossroads) -> IfaceToken<()> {
-    cr.register("pl.digitalradio", |b| {
-        b.method("wifi_on", ("data", ), ("response", ), handle_wifi_on);
-    })
-}
-
-fn handle_wifi_on(_ctx: &mut Context, _data: &mut (), (body,): (String,)) -> Result<(String,), MethodErr> {
-    let parsed_data: WifiOnData = match serde_json::from_str(body.as_str()) {
-        Ok(d) => d,
-        Err(_) => return Err(("pl.digitalradio.JsonDecodeError", "Could not decode args").into()),
-    };
-
-    println!("Connect to Wi-Fi [{}]", parsed_data.ssid);
-
-    let response = WifiOnResponse {
-        code: "ok".to_owned(),
-    };
-    let response_data = match json_encode(&response) {
-        Ok(d) => d,
-        Err(_) => return Err(("pl.digitalradio.JsonEncodeError", "Could not encode response").into()),
-    };
-    Ok((response_data,))
 }
