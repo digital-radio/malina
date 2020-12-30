c74c68daef60e60bc924320f0b3704d9186572a6
Michał Budziak <michal.budziak@saucelabs.com>
2020-12-30 17:17:08 +0100

    Remove container

--- development/run.sh
+++ development/run.sh
@@ -4,4 +4,4 @@ PWD=$(cd `dirname $0` && pwd)
 ROOT_PATH=$(dirname $PWD)
 
 docker build $PWD --file development/Dockerfile --tag malina-dev
-docker run -it -v $ROOT_PATH:/app --name malina-dev malina-dev bash
+docker run --rm -it -v $ROOT_PATH:/app --name malina-dev malina-dev bash
